1a420bed025ae223af214b94b08a69c1cdf98b7e
Andrew O'Brien <obrienan@uwm.edu>
2019-10-15 10:29:46 -0500

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# CASDA Python API
+
+Provides a simple Python API to access images hosted on the CSIRO ASKAP Science Data Archive ([CASDA](https://data.csiro.au/)).
---
+++ setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+with open("README.md") as f:
+    readme = f.read()
+
+setup(
+    name="casdapy",
+    version="0.1.0",
+    description="Provides functions to download data products from CASDA.",
+    long_description=readme,
+    author="Andrew O'Brien",
+    author_email="obrienan@uwm.edu",
+    url="",
+    packages=find_packages(),
+)
