d3cd7e15c6c890ac480aeebdfa931315e8580f81
Andrew O'Brien <obrienan@uwm.edu>
2019-10-28 16:32:43 -0500

    refactored and exposed more query options

--- casdapy/download.py
+++ casdapy/download.py
@@ -3,7 +3,7 @@ import getpass
 import logging
 from pathlib import Path
 
-from casdapy.images import CasdaSbidImageQuery
+from casdapy.query import CasdaSbidImageQuery, CasdaSbidCatalogueQuery
 
 logging.basicConfig(format="%(asctime)-15s %(module)s: %(levelname)s %(message)s")
 logger = logging.getLogger("casdapy")
@@ -13,19 +13,66 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("sbid", help="The ASKAP SBID.")
     parser.add_argument(
-        "--credentials_file",
+        "--image-type",
+        metavar="IMAGE_TYPE",
+        nargs="*",
+        help=(
+            "The image type(s) to download. Multiple can be given. No default: at least "
+            "one must be provided to download images. "
+            "Possible choices are: %(choices)s."
+        ),
+        choices=CasdaSbidImageQuery.IMAGE_CUBE_SUBTYPES,
+    ),
+    parser.add_argument(
+        "--image-pol",
+        metavar="POLARIZATION",
+        nargs="*",
+        help=(
+            "Image polarization product(s) to download. Multiple can be given. "
+            "Possible choices are: %(choices)s."
+        ),
+        choices=["I", "Q", "U", "V"],
+        default=["I"],
+    )
+    parser.add_argument(
+        "--catalogue-type",
+        metavar="CATALOGUE_TYPE",
+        nargs="*",
+        help=(
+            "The catalogue type(s) to download. Multiple can be given. No default: at "
+            "least one must be provided to download catalogues. "
+            "Possible choices are: %(choices)s."
+        ),
+        choices=CasdaSbidCatalogueQuery.CATALOGUE_SUBTYPES,
+    )
+    parser.add_argument(
+        "--credentials-file",
         type=Path,
         help=(
             "Read ATNF OPAL account credentials from a file containing the username "
             "and password on separate lines. If not supplied, user will be prompted to "
-            "enter these via stdin."
+            "enter these interactively."
         ),
     )
     parser.add_argument(
-        "--destination_dir",
+        "--destination-dir",
         type=Path,
         help="Directory to save downloaded images. Defaults to current directory.",
         default=".",
+    )
+    parser.add_argument(
+        "--poll-period",
+        type=int,
+        help="Number of seconds to poll CASDA for the job status for async jobs.",
+        default=30,
+    )
+    parser.add_argument(
+        "--dry-run",
+        action="store_true",
+        help=(
+            "Don't download any files, only perform query steps and report which files "
+            "*would* be downloaded."
+        ),
     )
     parser.add_argument(
         "--verbose",
@@ -44,10 +91,29 @@ def main():
     if args.verbose:
         logger.setLevel(logging.INFO)
 
-    casda_query = CasdaSbidImageQuery(args.sbid, opal_username, opal_password)
-    casda_query.run(args.destination_dir.expanduser().resolve())
+    if args.image_type:
+        casda_query = CasdaSbidImageQuery(
+            args.sbid,
+            opal_username,
+            opal_password,
+            polarizations=args.image_pol,
+            dataproducts=args.image_type,
+        )
+        casda_query.run(
+            args.destination_dir.expanduser().resolve(),
+            poll_period=args.poll_period,
+            dry_run=args.dry_run,
+        )
 
-    # TODO: verify checksums
+    if args.catalogue_type:
+        casda_query = CasdaSbidCatalogueQuery(
+            args.sbid, opal_username, opal_password, dataproducts=args.catalogue_type
+        )
+        casda_query.run(
+            args.destination_dir.expanduser().resolve(),
+            poll_period=args.poll_period,
+            dry_run=args.dry_run,
+        )
 
 
 if __name__ == "__main__":
