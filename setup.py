d62c204a5615feac13642df7d1b966133ab2a612
Andrew O'Brien <obrienan@uwm.edu>
2019-11-08 15:56:57 -0600

    added install requirements, set log level to INFO

--- setup.py
+++ setup.py
@@ -7,14 +7,22 @@ with open("README.md") as f:
 setup(
     name="casdapy",
     version="0.1.0",
-    description="Provides functions to download data products from CASDA.",
+    description="Provides functions to search for and download data products from CASDA.",
     long_description=readme,
     author="Andrew O'Brien",
     author_email="obrienan@uwm.edu",
-    url="",
+    url="https://github.com/askap-vast/casdapy",
+    python_requires=">=3.6",
+    install_requires=[
+        "astropy",
+        "click",
+        "pandas",
+        "requests",
+        "tqdm",
+    ],
     packages=find_packages(),
     package_data={
-        "scripts": ["logger_config.json"],
+        "casdapy.scripts": ["logger_config.json"],
     },
     entry_points={
         "console_scripts": ["casda_download=casdapy.scripts.download:main"],
