39ed7c0d78fb72839dd017c431eeaadcc63737c5
Dougal Dobie <ddobie@swin.edu.au>
2024-10-21 13:24:12 +1100

    Removed errant committed files

--- casdapy/test_function.py
+++
@@ -1,8 +0,0 @@
-from casdapy.casda import query_visibilities
-
-field_names = ["1806-25", "1739-25"]
-beams = [33,24]
-
-r = query_visibilities(fieldnames_like=field_names, beams=beams)
-
-print(r['filename'])
